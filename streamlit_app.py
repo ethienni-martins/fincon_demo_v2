a9386cada554ba66340f17e37a0b62da2e479465
Ethienni Martins <128940738+ethienni-martins@users.noreply.github.com>
2024-07-24 19:55:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.markdown(
     }
     div.stButton > button {
         width: auto;
-        min-width: 150px;
+        min-width: 165px;
         display: inline-block;
         text-align: center;
         margin-bottom: 5px;
