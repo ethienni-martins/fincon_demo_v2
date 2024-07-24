0de0e4893d24b61b3da14355383f2a2a92685ca0
Ethienni Martins <128940738+ethienni-martins@users.noreply.github.com>
2024-07-23 22:17:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.markdown(
         flex: 1;
     }
     div.stButton > button {
-        width: 80%;
+        width: 100%;
         display: inline-block;
         text-align: center;
     }
@@ -64,7 +64,7 @@ if st.session_state.selected_category:
     div.stButton > button[title="{}"] {{
         background-color: #4CAF50;
         color: white;
-        width: 80%;
+        width: 100%;
         display: inline-block;
         text-align: center;
     }}
